1b8262ab5f21e138a8324e72bb8a4c4d0fa0faad
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-10-05 11:29:56 +0100

    music_library app challenge

--- music_library/app.py
+++ music_library/app.py
@@ -2,25 +2,37 @@ from lib.database_connection import DatabaseConnection
 from lib.artist_repository import ArtistRepository
 from lib.album_repository import AlbumRepository
 
-# Connect to the database
-connection = DatabaseConnection()
-connection.connect()
+class Application():
+    def __init__(self):
+        self._connection = DatabaseConnection()
+        self._connection.connect()
+        self._connection.seed("seeds/music_library.sql")
 
-# Seed with some seed data
-connection.seed("seeds/music_library.sql")
+    def run(self):
 
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
+        while True:
+            print('\nWelcome to the music library manager!')
+            print('\nWhat would you like to do?')
+            print('  1 - List all albums')
+            print('  2 - List all artists')
 
-# List them out__di
-for artist in artists:
-    print(artist)
+            choice = input('\nEnter your choice: ')
 
-album_repository = AlbumRepository(connection)
-albums = album_repository.all()
+            if choice == '1':
+                album_repo = AlbumRepository(self._connection)
+                print('\nHere is the list of albums:')
+                for album in album_repo.all():
+                    print(f'{album.id} - {album.title}')
+                
+            
+            if choice == '2':
+                artist_repo = ArtistRepository(self._connection)
+                print('\nHere is the list of artists:')
+                for artist in artist_repo.all():
+                    print(f'{artist.id} - {artist.name}')
+            
+            break
 
-for album in albums:
-    print(album)
-
-print(album_repository.find(1))
+if __name__ == '__main__':
+    app = Application()
+    app.run()
