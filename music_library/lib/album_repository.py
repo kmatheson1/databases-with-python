0a23eebc5543a0703ad364bd6a120f20ba33eab1
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-10-05 10:28:16 +0100

    music library challenge complete

--- music_library/lib/album_repository.py
+++ music_library/lib/album_repository.py
@@ -16,3 +16,11 @@ class AlbumRepository():
         rows = self._connection.execute('SELECT * from albums WHERE id = %s', [album_id])
         row = rows[0]
         return Album(row["id"], row["title"], row["release_year"], row["artist_id"])
+    
+    def create(self, album):
+        self._connection.execute('INSERT INTO albums (title, release_year, artist_id) VALUES (%s, %s, %s)', [album.title, album.release_year, album.artist_id])
+        return None
+
+    def delete(self, album_id):
+        self._connection.execute('DELETE from albums where id = %s', [album_id])
+        return None
--- music_library/tests/test_album_repository.py
+++ music_library/tests/test_album_repository.py
@@ -37,3 +37,53 @@ def test_get_single_record(db_connection):
 
     album = repository.find(1)
     assert album == Album(1, 'Doolittle', 1989, 1)
+
+"""
+When we call ArtistRepository#create
+We get a new record in the database.
+"""
+def test_create_record(db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    repository = AlbumRepository(db_connection)
+
+    album = Album(None, "The Album", 1977, 2)
+    repository.create(album)
+
+    assert repository.all() == [
+        Album(1,'Doolittle', 1989, 1),
+        Album(2,'Surfer Rosa', 1988, 1),
+        Album(3,'Waterloo', 1974, 2),
+        Album(4,'Super Trouper', 1980, 2),
+        Album(5,'Bossanova', 1990, 1),
+        Album(6,'Lover', 2019, 3),
+        Album(7,'Folklore', 2020, 3),
+        Album(8,'I Put a Spell on You', 1965, 4),
+        Album(9,'Baltimore', 1978, 4),
+        Album(10,'Here Comes the Sun', 1971, 4),
+        Album(11,'Fodder on My Wings', 1982, 4),
+        Album(12,'Ring Ring', 1973, 2),
+        Album(13, "The Album", 1977, 2)
+    ]
+
+"""
+When we call ArtistRepository#delete
+We remove a record from the database.
+"""
+def test_delete_record(db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    repository = AlbumRepository(db_connection)
+
+    repository.delete(2)
+    assert repository.all() == [
+        Album(1,'Doolittle', 1989, 1),
+        Album(3,'Waterloo', 1974, 2),
+        Album(4,'Super Trouper', 1980, 2),
+        Album(5,'Bossanova', 1990, 1),
+        Album(6,'Lover', 2019, 3),
+        Album(7,'Folklore', 2020, 3),
+        Album(8,'I Put a Spell on You', 1965, 4),
+        Album(9,'Baltimore', 1978, 4),
+        Album(10,'Here Comes the Sun', 1971, 4),
+        Album(11,'Fodder on My Wings', 1982, 4),
+        Album(12,'Ring Ring', 1973, 2)
+    ]
